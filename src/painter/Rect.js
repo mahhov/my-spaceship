461310b10888f7f1248e40d16bb7ab8ebfb91f6e
mahhov <mahhov1@gmail.com>
2018-07-03 20:33:46 -0700

    make RectC decendent of Rect and add support for fill and color to Rect

--- src/painter/Rect.js
+++ src/painter/Rect.js
@@ -1,14 +1,25 @@
 class Rect {
-    constructor(x, y, width, height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
+	constructor(x, y, width, height, color, fill) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.color = color;
+		this.fill = fill;
+	}
 
-    paint(xt, yt, context) {
-        context.strokeRect(xt(this.x), yt(this.y), xt(this.width), yt(this.height));
-    }
+	paint(xt, yt, context) {
+		let tx = xt(this.x);
+		let ty = yt(this.y);
+		let tWidth = xt(this.width);
+		let tHeight = xt(this.height);
+
+		if (this.fill) {
+			context.fillStyle = this.color || '#000';
+			context.fillRect(tx, ty, tWidth, tHeight);
+		} else
+			context.strokeRect(tx, ty, tWidth, tHeight);
+	}
 }
 
 module.exports = Rect;
--- src/painter/RectC.js
+++ src/painter/RectC.js
@@ -1,24 +1,8 @@
-class RectC {
+const Rect = require('./Rect');
+
+class RectC extends Rect {
 	constructor(centerX, centerY, width, height, color, fill) {
-		this.centerX = centerX;
-		this.centerY = centerY;
-		this.width = width;
-		this.height = height;
-		this.color = color;
-		this.fill = fill;
-	}
-
-	paint(xt, yt, context) {
-		let tWidth = xt(this.width);
-		let tHeight = xt(this.height);
-		let x = xt(this.centerX) - tWidth / 2;
-		let y = yt(this.centerY) - tHeight / 2;
-
-		if (this.fill) {
-			context.fillStyle = this.color || '#000';
-			context.fillRect(x, y, tWidth, tHeight);
-		} else
-			context.strokeRect(x, y, tWidth, tHeight);
+		super(centerX - width / 2, centerY - height / 2, width, height, color, fill);
 	}
 }
 
