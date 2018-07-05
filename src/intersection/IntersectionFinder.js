746c3a8c584b8d6f748434e91d6383f13124140c
mahhov <mahhov1@gmail.com>
2018-07-04 17:14:25 -0700

    split out intersection finder canmove logic to helper methods and create collision table in readiness for per layered collisiions

--- src/intersection/IntersectionFinder.js
+++ src/intersection/IntersectionFinder.js
@@ -9,10 +9,36 @@ class IntersectionFinder {
 		this.HOSTILE_PROJECTILE = 3; // intersects with friendly units and passives
 		this.HOSTILE_UNIT = 4; // intersects with friendly units, friendly projectiles, and passives
 
-		this.layers = [this.PASSIVE, this.FRIENDLY_PROJECTILE, this.FRIENDLY_UNIT, this.HOSTILE_PROJECTILE, this.HOSTILE_UNIT];
+		let layers = [this.PASSIVE, this.FRIENDLY_PROJECTILE, this.FRIENDLY_UNIT, this.HOSTILE_PROJECTILE, this.HOSTILE_UNIT];
+		this.collisions = layers.map(() => []);
+		this.boundsGroups = layers.map(() => new LinkedList());
 
-		this.boundsGroups = {};
-		this.layers.forEach(layer => this.boundsGroups[layer] = new LinkedList());
+		this.initCollisions();
+	}
+
+	initCollisions() {
+		// passives intersect with everything
+		this.addCollision(this.PASSIVE, this.FRIENDLY_UNIT);
+		this.addCollision(this.PASSIVE, this.FRIENDLY_PROJECTILE);
+		this.addCollision(this.PASSIVE, this.FRIENDLY_UNIT);
+		this.addCollision(this.PASSIVE, this.HOSTILE_PROJECTILE);
+		this.addCollision(this.PASSIVE, this.HOSTILE_UNIT);
+
+		// friendly projectiles intersect with hostile units and passives
+		this.addCollision(this.FRIENDLY_PROJECTILE, this.HOSTILE_UNIT);
+
+		// friendly units intersect with hostile units, hostile projectiles, and passives
+		this.addCollision(this.FRIENDLY_UNIT, this.HOSTILE_UNIT);
+		this.addCollision(this.FRIENDLY_UNIT, this.HOSTILE_PROJECTILE);
+
+		// hostile projectiles intersect with friendly units and passives
+
+		// hostile uints intersects with friendly units, friendly projectiles, and passives
+	}
+
+	addCollision(layer1, layer2) {
+		this.collisions[layer1][layer2] = true;
+		this.collisions[layer2][layer1] = true;
 	}
 
 	addBounds(layer, bounds) {
@@ -36,7 +62,7 @@ class IntersectionFinder {
 		let vertical = dy <= 0 ? bounds.TOP : bounds.BOTTOM;
 
 		if (dx && dy) {
-			let [move, intersection] = this.canMoveTwoDirections(layer, bounds, dx, dy, magnitude, horizontal, vertical);
+			let [move, intersection] = this.checkMoveEntitiesIntersection(layer, bounds, dx, dy, magnitude, horizontal, vertical);
 
 			moveX += dx * move;
 			moveY += dy * move;
@@ -53,39 +79,41 @@ class IntersectionFinder {
 			}
 		}
 
-		let [move] = this.canMoveTwoDirections(layer, bounds, dx, dy, magnitude, horizontal, vertical);
+		let [move] = this.checkMoveEntitiesIntersection(layer, bounds, dx, dy, magnitude, horizontal, vertical);
 		moveX += dx * move;
 		moveY += dy * move;
 
 		return [moveX, moveY];
 	}
 
-	canMoveTwoDirections(layer, bounds, dx, dy, magnitude, horizontal, vertical) {
-		let intersection; // 0 = none, 1 = horizontal, 2 = vertical
+	checkMoveEntitiesIntersection(layer, bounds, dx, dy, magnitude, horizontal, vertical) {
+		let magnitudeIntersectionPair = [magnitude]; // 2nd index for intersection: 0 = none, 1 = horizontal, 2 = vertical
 
 		// todo dont hardcode psasive
 		this.boundsGroups[this.PASSIVE].forEach(iBounds => {
-			let horizontalDelta = this.getDelta(horizontal, dx, bounds, iBounds, false);
-			let verticalDelta = this.getDelta(vertical, dy, bounds, iBounds, false);
-
-			if (horizontalDelta >= magnitude || verticalDelta >= magnitude || horizontalDelta < 0 && verticalDelta < 0)
-				return;
-
-			let [maxDelta, whichDelta] = maxWhich(horizontalDelta, verticalDelta);
-
-			let horizontalFarDelta = this.getDelta(horizontal, dx, bounds, iBounds, true);
-			let verticalFarDelta = this.getDelta(vertical, dy, bounds, iBounds, true);
-
-			if (maxDelta >= 0 && maxDelta < Math.min(horizontalFarDelta, verticalFarDelta)) {
-				magnitude = maxDelta;
-				intersection = whichDelta + 1;
-			}
+			magnitudeIntersectionPair = IntersectionFinder.checkMoveEntityIntersection(bounds, dx, dy, magnitude, horizontal, vertical, iBounds) || magnitudeIntersectionPair;
 		});
 
-		return [magnitude, intersection];
+		return magnitudeIntersectionPair;
 	}
 
-	getDelta(direction, d, bounds, iBounds, far) {
+	static checkMoveEntityIntersection(bounds, dx, dy, magnitude, horizontal, vertical, iBounds) {
+		let horizontalDelta = IntersectionFinder.getDelta(horizontal, dx, bounds, iBounds, false);
+		let verticalDelta = IntersectionFinder.getDelta(vertical, dy, bounds, iBounds, false);
+
+		if (horizontalDelta >= magnitude || verticalDelta >= magnitude || horizontalDelta < 0 && verticalDelta < 0)
+			return;
+
+		let [maxDelta, whichDelta] = maxWhich(horizontalDelta, verticalDelta);
+
+		let horizontalFarDelta = IntersectionFinder.getDelta(horizontal, dx, bounds, iBounds, true);
+		let verticalFarDelta = IntersectionFinder.getDelta(vertical, dy, bounds, iBounds, true);
+
+		if (maxDelta >= 0 && maxDelta < Math.min(horizontalFarDelta, verticalFarDelta))
+			return [maxDelta, whichDelta + 1];
+	}
+
+	static getDelta(direction, d, bounds, iBounds, far) {
 		if (d) {
 			if (far)
 				direction = bounds.oppositeDirection(direction);
