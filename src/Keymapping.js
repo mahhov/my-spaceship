652c0adee1e0fb46bdf30c54975ced67c1045933
mahhov <mahhov1@gmail.com>
2018-07-07 09:58:12 -0700

    fix keymapping considering release key state as active

--- src/Keymapping.js
+++ src/Keymapping.js
@@ -1,25 +1,26 @@
 class Keymapping {
-    constructor() {
-        this.MOVE_LEFT = 0;
-        this.MOVE_UP = 1;
-        this.MOVE_RIGHT = 2;
-        this.MOVE_DOWN = 3;
+	constructor() {
+		this.MOVE_LEFT = 0;
+		this.MOVE_UP = 1;
+		this.MOVE_RIGHT = 2;
+		this.MOVE_DOWN = 3;
 
-        this.map = {};
+		this.map = {};
 
-        this.map[this.MOVE_LEFT] = 'a';
-        this.map[this.MOVE_UP] = 'w';
-        this.map[this.MOVE_RIGHT] = 'd';
-        this.map[this.MOVE_DOWN] = 's';
-    }
+		this.map[this.MOVE_LEFT] = 'a';
+		this.map[this.MOVE_UP] = 'w';
+		this.map[this.MOVE_RIGHT] = 'd';
+		this.map[this.MOVE_DOWN] = 's';
+	}
 
-    getKey(control) {
-        return this.map[control];
-    }
+	getKey(control) {
+		return this.map[control];
+	}
 
-    isActive(controller, control) {
-        return controller.getKey(this.getKey(control));
-    }
+	isActive(controller, control) {
+		let key = controller.getKey(this.getKey(control));
+		return key === controller.PRESSED || key === controller.DOWN;
+	}
 }
 
 module.exports = Keymapping;
