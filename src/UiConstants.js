d9cd3f7f26c556b25e3d529dc6c4de982372bc3d
mahhov <mahhov1@gmail.com>
2018-07-12 15:22:11 -0700

    minor add todo to find pretier colors

--- src/UiConstants.js
+++ src/UiConstants.js
@@ -18,7 +18,6 @@ Colors.BASIC_ATTACK_EMPTY_COLOR = Colors.BASIC_ATTACK_COLOR.multiply(EMPTY_MULT)
 Colors.DASH_EMPTY_COLOR = Colors.DASH_COLOR.multiply(EMPTY_MULT);
 Colors.HEAL_EMPTY_COLOR = Colors.HEAL_COLOR.multiply(EMPTY_MULT);
 
-
 const Positions = {
 	MARGIN: .02,
 	BAR_HEIGHT: .02
@@ -26,11 +25,21 @@ const Positions = {
 
 module.exports = {UiCs: Colors, UiPs: Positions};
 
-// RESERVE_COLOR: Color.from1(.2, .6, .6),
+// Notes
+
 // SHIELD_COLOR: Color.from1(.4, .5, .7),
+// RESERVE_COLOR: Color.from1(.2, .6, .6),
 // EXPERIENCE_COLOR: Color.from1(.9, .6, .1),
 
 // LIFE_EMPTY_COLOR: Color.fromHex(0x4, 0xB, 0xC, true),
 // LIFE_FILL_COLOR: Color.fromHex(0x5, 0xD, 0xF, true),
 // STAMINA_EMPTY_COLOR: Color.fromHex(0xc, 0xc, 0x4, true),
 // STAMINA_FILL_COLOR: Color.fromHex(0xf, 0xf, 0x5, true),
+
+const localLife = "#cc4e4e";
+const localStamina = "#ffcc99";
+const localShield = "#6680b3";
+const localReserve = "#339999";
+const localExperience = "#e6991a";
+
+// todo find prettier colors
