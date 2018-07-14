effebf21e4dcca89a6669d74e5e9b4b7404241a8
mahhov <mahhov1@gmail.com>
2018-07-14 16:11:37 -0700

    monster enrage to spew more projectiles, of greater spread, and more proximity degen

--- src/entities/Monster.js
+++ src/entities/Monster.js
@@ -12,9 +12,13 @@ class Monster extends LivingEntity {
 	constructor(x, y) {
 		super(x, y, .04, .004, Color.fromHex(0x9, 0x0, 0x4, true), IntersectionFinderLayers.HOSTILE_UNIT);
 		this.attackPhase = new Phase(100, 100, 200);
-		this.enragePhase = new Phase(7000);
+		this.enragePhase = new Phase(6000);
 		this.enragePhase.setPhase(0);
 		this.degenRange = .33;
+	}
+
+	isEnraged() {
+		return this.enragePhase.isComplete();
 	}
 
 	update(logic, intersectionFinder, player) {
@@ -30,16 +34,17 @@ class Monster extends LivingEntity {
 	distanceDegen(logic, intersectionFinder, player) {
 		let playerDistance = getRectDistance(player.x - this.x, player.y - this.y);
 		if (playerDistance < this.degenRange)
-			player.changeHealth(-.002);
+			player.changeHealth(-.002 * (this.isEnraged() * 4 + 1));
 	}
 
 	projecitlePhase(logic, intersectionFinder, player) {
-		if (Math.random() > .1)
+		if (!this.isEnraged() && Math.random() > .1)
 			return;
 
 		for (let i = 0; i < 10; i++) {
 			let [dx, dy] = setMagnitude(player.x - this.x, player.y - this.y, .015);
-			let [rdx, rdy] = setMagnitude(...thetaToUnitVector(Math.random() * Math.PI * 2), .015 * Math.random() * .2);
+			let spread = .003 * (this.isEnraged() * 2 + 1);
+			let [rdx, rdy] = setMagnitude(...thetaToUnitVector(Math.random() * Math.PI * 2), Math.random() * spread);
 
 			let projectile = new Projectile(this.x, this.y, .01, .01, dx + rdx, dy + rdy, 100, .005, false);
 			logic.addProjectile(projectile);
