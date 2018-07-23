18b5c8bad3863d60d756a579b6d943310f429eb7
mahhov <mahhov1@gmail.com>
2018-07-23 16:07:07 -0700

    extract boss1 projetile phase into rersuable shotgun module.

---
+++ src/entities/module/Shotgun.js
@@ -0,0 +1,35 @@
+const makeEnum = require('../../util/Enum');
+const AttackModule = require('./AttackModule');
+const {setMagnitude, thetaToUnitVector} = require('../../util/Number');
+const Projectile = require('../attack/Projectile');
+
+const Stages = makeEnum('ACTIVE', 'INACTIVE');
+
+class Shotgun extends AttackModule {
+	config(rate, count, velocity, spread, duration, damage, origin) {
+		this.rate = rate;
+		this.count = count;
+		this.velicity = velocity;
+		this.spread = spread;
+		this.duration = duration;
+		this.damage = damage;
+		this.origin = origin;
+	}
+
+	apply(logic, intersectionFinder, target) {
+		if (this.stage !== Stages.ACTIVE || Math.random() > this.rate)
+			return;
+
+		for (let i = 0; i < this.count; i++) {
+			let [dx, dy] = setMagnitude(target.x - this.origin.x, target.y - this.origin.y, this.velicity);
+			let [rdx, rdy] = setMagnitude(...thetaToUnitVector(Math.random() * Math.PI * 2), Math.random() * this.spread);
+
+			let projectile = new Projectile(this.origin.x, this.origin.y, .01, .01, dx + rdx, dy + rdy, this.duration, this.damage, false);
+			logic.addProjectile(projectile);
+		}
+	}
+}
+
+Shotgun.Stages = Stages;
+
+module.exports = Shotgun;
--- src/entities/monsters/Boss1.js
+++ src/entities/monsters/Boss1.js
@@ -2,11 +2,9 @@ const Monster = require('./Monster');
 const Color = require('../../util/Color');
 const Phase = require('../../util/Phase');
 const NearbyDegen = require('../module/NearbyDegen');
+const Shotgun = require('../module/Shotgun');
 const StarShip = require('../../graphics/StarShip');
-const {getRectDistance, getMagnitude, setMagnitude, thetaToUnitVector} = require('../../util/Number');
-const Projectile = require('../attack/Projectile');
 const {UiCs, UiPs} = require('../../UiConstants');
-const RectC = require('../../painter/RectC');
 const Bar = require('../../painter/Bar');
 
 const PRE_DEGEN_PHASE = 0, DEGEN_PHASE = 1, PROJECTILE_PHASE = 2;
@@ -23,6 +21,11 @@ class Boss1 extends Monster {
 		this.nearbyDegen.config(.33, .002, this);
 		this.addModule(this.nearbyDegen);
 
+		this.shotgun = new Shotgun();
+		this.shotgun.setStagesMapping({[PRE_DEGEN_PHASE]: Shotgun.Stages.INACTIVE, [DEGEN_PHASE]: Shotgun.Stages.INACTIVE, [PROJECTILE_PHASE]: Shotgun.Stages.ACTIVE});
+		this.shotgun.config(.1, 10, .015, .003, 100, .005, this);
+		this.addModule(this.shotgun);
+
 		this.ship = new StarShip(this.width, this.height, {fill: true, color: this.color.get()});
 	}
 
@@ -34,32 +37,16 @@ class Boss1 extends Monster {
 		if (this.attackPhase.sequentialTick())
 			this.modulesSetStage(this.attackPhase.get());
 
-		if (this.enragePhase.tick())
+		if (this.enragePhase.tick()) {
 			this.nearbyDegen.config(.33, .01, this);
+			this.shotgun.config(1, 10, .015, .009, 100, .005, this);
+		}
 
 		this.modulesApply(logic, intersectionFinder, player);
-
-		if (this.attackPhase.get() === PROJECTILE_PHASE)
-			this.projectilePhase(logic, intersectionFinder, player);
-	}
-
-	projectilePhase(logic, intersectionFinder, player) {
-		if (!this.isEnraged() && Math.random() > .1)
-			return;
-
-		for (let i = 0; i < 10; i++) {
-			let [dx, dy] = setMagnitude(player.x - this.x, player.y - this.y, .015);
-			let spread = .003 * (this.isEnraged() * 2 + 1);
-			let [rdx, rdy] = setMagnitude(...thetaToUnitVector(Math.random() * Math.PI * 2), Math.random() * spread);
-
-			let projectile = new Projectile(this.x, this.y, .01, .01, dx + rdx, dy + rdy, 100, .005, false);
-			logic.addProjectile(projectile);
-		}
 	}
 
 	paint(painter, camera) {
 		this.ship.paint(painter, camera, this.x, this.y, [0, 1]);
-
 		this.modulesPaint(painter, camera);
 	}
 
