d03d48a51f0292bf6847d22ae32d80e22afee9dc
mahhov <mahhov1@gmail.com>
2018-07-31 16:08:55 -0700

    expand utility charge class in preperateion to be used to replace ability cooldown and charges

--- src/util/Charge.js
+++ src/util/Charge.js
@@ -6,24 +6,35 @@ class Charge {
 		this.generateRate = generateRate;
 	}
 
-	generate() {
-		this.change(this.generateRate);
+	// return true if reached 0 or max
+	generate() { // todo [high] rename increment
+		return this.change(this.generateRate);
 	}
 
 	restore() {
 		this.value = this.max;
 	}
 
+	// return true if reached 0 or max
 	change(amount) {
 		this.value = clamp(this.value + amount, 0, this.max);
+		return this.value === 0 || this.value === this.max;
 	}
 
 	get() {
 		return this.value;
 	}
 
+	getMissing() {
+		return this.max - this.value;
+	}
+
 	getRatio() {
 		return this.value / this.max;
+	}
+
+	getMissingRatio() {
+		return this.getMissing() / this.max;
 	}
 
 	isFull() {
