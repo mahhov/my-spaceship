e2c9677b9535ed3cf65de95464c4e3465afe55ac
mahhov <mahhov1@gmail.com>
2018-08-04 11:10:44 -0700

    make rocks white to be visible over starfield background

--- src/entities/Rock.js
+++ src/entities/Rock.js
@@ -1,11 +1,12 @@
 const Entity = require('./Entity');
 const IntersectionFinder = require('../intersection/IntersectionFinder');
+const Color = require('../util/Color');
 const RockGraphics = require('../graphics/RockGraphic');
 
 class Rock extends Entity {
 	constructor(x, y, size) {
 		super(x, y, size, size, IntersectionFinder.Layers.PASSIVE);
-		this.setGraphics(new RockGraphics(size, size));
+		this.setGraphics(new RockGraphics(size, size, {fill: true, color: Color.fromHex(0xf, 0xf, 0xf, true).get()}));
 	}
 }
 
