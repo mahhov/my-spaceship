27d9cc66960a89d91342f52b97488ae503a78857
mahhov <mahhov1@gmail.com>
2018-08-04 13:02:58 -0700

    noise demo to support keyboard input for modifying noise range paramter and

--- src/logic/NoiseDemo.js
+++ src/logic/NoiseDemo.js
@@ -3,21 +3,24 @@ const {NoiseSimplex, NoiseGradient} = require('../util/Noise');
 const {rand} = require('../util/Number');
 const Controller = require('../control/Controller');
 const Rect = require('../painter/Rect');
+const RectC = require('../painter/RectC');
+const Text = require('../painter/Text');
 
 const THRESHOLD = .5;
 const N = 200; // resolution
 const NTH = 1 / N;
-const NOISE_RANGE = 20; // feature sizes, bigger NOISE_RANGE means smaller features
+const DEFAULT_NOISE_RANGE = 20; // feature sizes, bigger noiseRange means smaller features
 
 class NoiseDemo extends Logic {
 	constructor(controller, painter) {
 		super(controller, painter);
+		this.noiseRange = DEFAULT_NOISE_RANGE;
 		this.reset();
 	}
 
 	reset() {
 		this.results = [];
-		let noise = new NoiseSimplex(NOISE_RANGE);
+		let noise = new NoiseSimplex(this.noiseRange);
 		for (let x = 0; x < N; x++) {
 			this.results[x] = [];
 			for (let y = 0; y < N; y++) {
@@ -29,14 +32,31 @@ class NoiseDemo extends Logic {
 	}
 
 	iterate() {
+		this.control();
+		this.paint();
+	}
+
+	control() {
+		if (this.controller.getKeyState('arrowdown') === Controller.KeyStates.PRESSED)
+			this.noiseRange -= 5;
+		if (this.controller.getKeyState('arrowup') === Controller.KeyStates.PRESSED)
+			this.noiseRange += 5;
+		if (this.controller.getKeyState('arrowleft') === Controller.KeyStates.PRESSED)
+			this.noiseRange--;
+		if (this.controller.getKeyState('arrowright') === Controller.KeyStates.PRESSED)
+			this.noiseRange++;
 		if (this.controller.getKeyState(' ') === Controller.KeyStates.PRESSED)
 			this.reset();
+	}
 
+	paint() {
 		for (let x = 0; x < N; x++)
 			for (let y = 0; y < N; y++) {
 				if (this.results[x][y]) {
 					let c = 0;
 					this.painter.add(new Rect(x * NTH, y * NTH, 1 / N, 1 / N, {fill: true, color: `rgb(${c}, ${c}, ${c})`}));
+					this.painter.add(new RectC(.1, .1, .03, .03, {fill: true, color: `#fff`}));
+					this.painter.add(new Text(.1, .1, this.noiseRange));
 				}
 			}
 	}
