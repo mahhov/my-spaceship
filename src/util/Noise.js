4f20379c06d83f919012fc6b9cd5adad8120d9fc
mahhov <mahhov1@gmail.com>
2018-08-04 13:01:34 -0700

    expand noiseSimplex to support distribution noise

--- src/util/Noise.js
+++ src/util/Noise.js
@@ -3,13 +3,33 @@ const SimplexNoise = require('simplex-noise');
 const {EPSILON, getMagnitude, rand} = require('./Number');
 
 class NoiseSimplex {
-	constructor(scale) {
+	constructor(scale = 10, threshold = .5, thresholdRandWeight = 1) {
 		this.scale = scale;
+		this.threshold = threshold;
+		this.thresholdRandWeight = thresholdRandWeight;
 		this.simplexNoise = new SimplexNoise(rand);
 	}
 
 	get(x, y) {
 		return this.simplexNoise.noise2D(x * this.scale + 1, y * this.scale) * .5 + .5; // seems like simplexNoise implementation is bugged to always return 0 at (0, 0)
+	}
+
+	// not consistent, calling it multiple times with same paramters can yield different results
+	getB(x, y) {
+		return this.get(x, y) > this.threshold + rand(this.thresholdRandWeight);
+	}
+
+	// retun count number of points within range [[0 - width], [0 - height]], structured as 2d array
+	// not consistent, calling it multiple times with same paramters can yield different results
+	positions(count, width, height) {
+		let positions = [];
+		while (positions.length < count) {
+			let x = rand();
+			let y = rand();
+			if (this.getB(x, y))
+				positions.push([x * width, y * height]);
+		}
+		return positions;
 	}
 }
 
