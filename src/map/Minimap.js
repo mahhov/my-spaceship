6e5dda53224036e8f40a1dbdec89bae6a6fc11ab
mahhov <mahhov1@gmail.com>
2018-08-05 10:20:38 -0700

    make minmap.paintdot static

--- src/map/Minimap.js
+++ src/map/Minimap.js
@@ -32,12 +32,12 @@ class Minimap {
 
 		let camera = this.createCamera();
 		painter.add(Rect.withCamera(camera, 0, 0, this.map.width, this.map.height, {fill: true, color: BACKGROUND_COLOR}));
-		this.map.rocks.forEach(rock => this.paintDot(painter, camera, rock.x, rock.y, ROCK_COLOR));
-		this.paintDot(painter, camera, this.map.player.x, this.map.player.y, PLAYER_COLOR);
-		this.map.monsters.forEach(monster => this.paintDot(painter, camera, monster.x, monster.y, MONSTER_COLOR));
+		this.map.rocks.forEach(rock => Minimap.paintDot(painter, camera, rock.x, rock.y, ROCK_COLOR));
+		Minimap.paintDot(painter, camera, this.map.player.x, this.map.player.y, PLAYER_COLOR);
+		this.map.monsters.forEach(monster => Minimap.paintDot(painter, camera, monster.x, monster.y, MONSTER_COLOR));
 	}
 
-	paintDot(painter, camera, x, y, color) {
+	static paintDot(painter, camera, x, y, color) {
 		const DOT_SIZE = .2;
 		painter.add(RectC.withCamera(camera, x, y, DOT_SIZE, DOT_SIZE, {fill: true, color}));
 	}
