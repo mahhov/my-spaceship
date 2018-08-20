05d4ee77db9d5012ba1f07ee99a75868349b518d
mahhov <mahhov1@gmail.com>
2018-08-20 15:25:11 -0700

    remove turret code from outpost portal

--- src/entities/monsters/OutpostPortal.js
+++ src/entities/monsters/OutpostPortal.js
@@ -1,35 +1,14 @@
-const makeEnum = require('../../util/Enum');
 const Monster = require('./Monster');
 const {UiCs} = require('../../util/UiConstants');
-const Phase = require('../../util/Phase');
-const NearbyDegen = require('../module/NearbyDegen');
-const StarShip = require('../../graphics/StarShip');
-
-const Phases = makeEnum('REST', 'ATTACK');
+const OutpostPortalGraphic = require('../../graphics/OutpostPortalGraphic');
 
 class OutpostPortal extends Monster {
 	constructor(x, y) {
 		super(x, y, .04, .04, .04);
-		this.setGraphics(new StarShip(this.width, this.height, {fill: true, color: UiCs.Entity.MONSTER.get()}));
-
-		this.attackPhase = new Phase(200, 200);
-		this.attackPhase.setRandomTick();
-
-		let nearbyDegen = new NearbyDegen();
-		nearbyDegen.setStagesMapping({
-			[Phases.REST]: NearbyDegen.Stages.INACTIVE,
-			[Phases.ATTACK]: NearbyDegen.Stages.ACTIVE
-		});
-		nearbyDegen.config(this, .4, .001);
-		this.moduleManager.addModule(nearbyDegen);
-
-		this.moduleManager.modulesSetStage(this.attackPhase.get());
+		this.setGraphics(new OutpostPortalGraphic(this.width, this.height, {fill: true, color: UiCs.Entity.MONSTER.get()}));
 	}
 
 	update(map, intersectionFinder, monsterKnowledge) {
-		if (this.attackPhase.sequentialTick())
-			this.moduleManager.modulesSetStage(this.attackPhase.get());
-		this.moduleManager.modulesApply(map, intersectionFinder, monsterKnowledge.getPlayer());
 	}
 }
 
