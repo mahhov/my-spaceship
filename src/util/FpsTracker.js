00ab0e17ef771a9d97a7cbf2dde281dc82113b26
mahhov <mahhov1@gmail.com>
2018-08-24 06:20:44 -0700

    fps tracker

---
+++ src/util/FpsTracker.js
@@ -0,0 +1,23 @@
+class FpsTracker {
+	constructor() {
+		this.fps = 0;
+	}
+
+	tick() {
+		let now = performance.now();
+		let passed = now - this.start;
+		if (!(passed < 1000)) {
+			this.start = now;
+			this.fps = this.ticks * 1000 / passed;
+			this.ticks = 0;
+		}
+		this.ticks++;
+	}
+
+	getFps() {
+		this.tick();
+		return Math.round(this.fps);
+	}
+}
+
+module.exports = FpsTracker;
--- src/view/Canvas.js
+++ src/view/Canvas.js
@@ -1,11 +1,13 @@
 const Controller = require('../control/Controller');
 const PainterCompositor = require('../painter/PainterCompositor');
+const FpsTracker = require('../util/FpsTracker');
 const Game = require('../logic/Game');
 const GraphicsDemo = require('../logic/GraphicsDemo');
 const StarfieldDemo = require('../logic/StarfieldDemo');
 const NoiseDemo = require('../logic/NoiseDemo');
 const MapDemo = require('../logic/MapDemo');
 const InterfaceDemo = require('../logic/InterfaceDemo');
+const Text = require('../painter/Text');
 
 const sleep = milli =>
 	new Promise(resolve => setTimeout(resolve, milli));
@@ -13,6 +15,7 @@ const sleep = milli =>
 const canvas = document.getElementById('canvas');
 const controller = new Controller(canvas);
 const painterSet = new PainterCompositor(canvas);
+const fpsTracker = new FpsTracker();
 
 const game = new Game(controller, painterSet);
 const graphicsDemo = new GraphicsDemo(controller, painterSet);
@@ -27,6 +30,7 @@ let loop = async () => {
 	while (true) {
 		painterSet.clear();
 		logic.iterate();
+		painterSet.uiPainter.add(new Text(.97, .03, fpsTracker.getFps()));
 		painterSet.paint();
 		controller.expire();
 		await sleep(10);
