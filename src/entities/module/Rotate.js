477608f7134df064aab9d9c1df2ebe2c9f65abac
mahhov <mahhov1@gmail.com>
2018-10-12 15:38:41 -0700

    rotate module

---
+++ src/entities/module/Rotate.js
@@ -0,0 +1,21 @@
+const makeEnum = require('../../util/Enum');
+const Module = require('./Module');
+const {thetaToVector} = require('../../util/Number');
+
+const Stages = makeEnum('ACTIVE', 'INACTIVE');
+
+class Rotate extends Module {
+	config(origin, rate = 1 / 50, theta = 0) {
+		this.origin = origin;
+		this.rate = rate;
+		this.theta = theta;
+	}
+
+	apply(map, intersectionFinder, target) {
+		[this.origin.moveDirection.x, this.origin.moveDirection.y] = thetaToVector(this.theta += this.rate);
+	}
+}
+
+Rotate.Stages = Stages;
+
+module.exports = Rotate;
--- src/entities/monsters/OutpostPortal.js
+++ src/entities/monsters/OutpostPortal.js
@@ -1,27 +1,38 @@
+const makeEnum = require('../../util/Enum');
 const Monster = require('./Monster');
 const {Colors} = require('../../util/Constants');
 const OutpostPortalGraphic = require('../../graphics/OutpostPortalGraphic');
-const {thetaToVector, rand, randVector} = require('../../util/Number');
+const Phase = require('../../util/Phase');
+const Rotate = require('../module/Rotate');
+const Spawn = require('../module/Spawn');
 const MeleeDart = require('./MeleeDart');
+
+const Phases = makeEnum('ONE');
 
 class OutpostPortal extends Monster {
 	constructor(x, y) {
 		super(x, y, .04, .04, .04);
 		this.setGraphics(new OutpostPortalGraphic(this.width, this.height, {fill: true, color: Colors.Entity.MONSTER.get()}));
-		this.theta = 0;
+
+		this.attackPhase = new Phase(0);
+
+		let rotate = new Rotate();
+		rotate.setStagesMapping({[Phases.ONE]: Rotate.Stages.ACTIVE});
+		rotate.config(this);
+		this.moduleManager.addModule(rotate);
+
+		let spawn = new Spawn();
+		spawn.setStagesMapping({[Phases.ONE]: Spawn.Stages.ACTIVE});
+		spawn.config(this, .2, .005, 3, 6, MeleeDart);
+		this.moduleManager.addModule(spawn);
+
+		this.moduleManager.modulesSetStage(this.attackPhase.get());
 	}
 
 	update(map, intersectionFinder, monsterKnowledge) {
-		[this.moveDirection.x, this.moveDirection.y] = thetaToVector(this.theta++ / 50);
-		// todo [high] make this into a spawn module
-		if (Math.random() > .005)
-			return;
-		let turrets = 3 + rand(3);
-		for (let i = 0; i < turrets; i++) {
-			let spawnVector = randVector(.2);
-			let meleeDart = new MeleeDart(this.x + spawnVector[0], this.y + spawnVector[1]);
-			map.addMonster(meleeDart);
-		}
+		if (this.attackPhase.sequentialTick())
+			this.moduleManager.modulesSetStage(this.attackPhase.get());
+		this.moduleManager.modulesApply(map, intersectionFinder, monsterKnowledge.getPlayer());
 	}
 }
 
