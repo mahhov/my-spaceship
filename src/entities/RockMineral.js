b91735803235c2c373dcace3981743777ca53fe9
manuk <mahhov1@gmail.com>
2019-11-07 16:52:13 -0800

    minor typo RockGraphics -> RockGraphic

--- src/entities/RockMineral.js
+++ src/entities/RockMineral.js
@@ -1,12 +1,12 @@
 const Entity = require('./Entity');
 const IntersectionFinder = require('../intersection/IntersectionFinder');
 const {Colors} = require('../util/Constants');
-const RockGraphics = require('../graphics/RockGraphic');
+const RockGraphic = require('../graphics/RockGraphic');
 
 class RockMineral extends Entity {
 	constructor(x, y, size) {
 		super(x, y, size, size, IntersectionFinder.Layers.PASSIVE);
-		this.setGraphics(new RockGraphics(size, size, {fill: true, color: Colors.Entity.ROCK_MINERAL.get()}));
+		this.setGraphics(new RockGraphic(size, size, {fill: true, color: Colors.Entity.ROCK_MINERAL.get()}));
 	}
 }
 
