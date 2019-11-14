4e57ce66ae336f176f5dcdfb7f70b07476f3f545
manuk <mahhov1@gmail.com>
2019-11-13 16:15:00 -0800

    fix some chase bugs regarding delta magnitude, use number.cos/sin, and add reverse option

--- src/entities/module/Chase.js
+++ src/entities/module/Chase.js
@@ -1,8 +1,9 @@
 const makeEnum = require('../../util/Enum');
 const Module = require('./Module');
+const {cos, sin} = require('../../util/Number');
 const Vector = require('../../util/Vector');
 
-const Stages = makeEnum('ACTIVE', 'INACTIVE');
+const Stages = makeEnum('ACTIVE', 'INACTIVE', 'REVERSE');
 
 class Chase extends Module {
 	config(origin, speed, skirmishTime, skirmishDistance, rotationSpeed) {
@@ -10,14 +11,17 @@ class Chase extends Module {
 		this.speed = speed;
 		this.skirmishTime = skirmishTime;
 		this.skirmishDistance = skirmishDistance;
-		this.rotationSpeed = rotationSpeed; // 0 means instant rotation
+		this.rotationSpeedCos = cos(rotationSpeed); // 0 rotationSpeed means instant rotation
+		this.rotationSpeedSin = sin(rotationSpeed);
 	}
 
 	apply_(map, intersectionFinder, target) {
-		if (this.stage !== Stages.ACTIVE)
+		if (this.stage === Stages.INACTIVE)
 			return;
 
 		let delta = Vector.fromObj(target).subtract(Vector.fromObj(this.origin));
+		if (this.stage === Stages.REVERSE)
+			delta.negate();
 
 		if (this.skirmishTime) {
 			if (!this.skirmishTick) {
@@ -30,12 +34,15 @@ class Chase extends Module {
 			delta.add(this.skirmishVec);
 		}
 
-		if (!this.rotation)
+		if (!this.rotation) {
 			this.rotation = Vector.fromObj(delta);
-		else if (this.rotationSpeed)
-			this.rotation.rotateByCosSinTowards(Math.cos(this.rotationSpeed), Math.sin(this.rotationSpeed), delta);
-		else
+			this.rotation.magnitude = 1;
+		} else if (this.rotationSpeed)
+			this.rotation.rotateByCosSinTowards(this.rotationSpeedCos, this.rotationSpeedSin, delta);
+		else {
 			this.rotation = delta;
+			this.rotation.magnitude = 1;
+		}
 
 		this.origin.safeMove(intersectionFinder, this.rotation.x, this.rotation.y, this.speed);
 	}
