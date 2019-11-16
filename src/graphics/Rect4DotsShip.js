a89cfeaf616801eea69defbd65f577ee9a8e2c30
manuk <mahhov1@gmail.com>
2019-11-15 22:54:11 -0800

    modify color, fill, and dots of Rect4DotsShip graphics

--- src/graphics/Rect4DotsShip.js
+++ src/graphics/Rect4DotsShip.js
@@ -7,23 +7,21 @@ const RECT_POINTS = [
 	[1, 0],
 	[0, -1],
 	[-1, 0]];
-const RECT_COLOR = Color.from255(240, 200, 230).get();
 
-const DOT_SCALE = .2;
-const DOT_POS = .3;
+const DOT_SCALE = .15;
+const DOT_POS = .25;
 const DOT_POINTS = PathCreator.createCirclePoints(1, 6, 0, 0);
 const DOT_COLOR = Color.from1(1, 1, 1).get();
 
 class Rect4DotsShip extends Graphics {
-	constructor(width, height) {
+	constructor(width, height, color) {
 		super();
-		this.addPath(width, height, RECT_POINTS, true,
-			{...graphicOptions, fill: 'double', color: RECT_COLOR});
+		this.addPath(width, height, RECT_POINTS, true, {fill: true, color});
 		RECT_POINTS.forEach(([x, y]) =>
 			this.addPathXY(
 				x * width * DOT_POS, y * height * DOT_POS,
 				width * DOT_SCALE, height * DOT_SCALE,
-				DOT_POINTS, true, {fill: 'double', color: DOT_COLOR}));
+				DOT_POINTS, true, {fill: true, color: DOT_COLOR}));
 	}
 }
 
