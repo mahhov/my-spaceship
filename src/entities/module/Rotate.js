2632dbf53bd1da2417d7070bde9921eb259fcc3c
manuk <mahhov1@gmail.com>
2019-11-15 23:04:41 -0800

    fix rotate module to use setMoveDirection method instead of explicitly setting move direction values on entity, to fix bug with unnormalized values scaling paint size

--- src/entities/module/Rotate.js
+++ src/entities/module/Rotate.js
@@ -1,20 +1,28 @@
 const makeEnum = require('../../util/Enum');
 const Module = require('./Module');
+const Vector = require('../../util/Vector');
 const {thetaToVector} = require('../../util/Number');
 
 const Stages = makeEnum('ACTIVE', 'INACTIVE');
 
 class Rotate extends Module {
-	config(origin, rate = 1 / 50, theta = 0) {
+	config(origin, rate = 1 / 50, theta = 0, atTarget = false) {
 		this.origin = origin;
 		this.rate = rate;
 		this.theta = theta;
+		this.atTarget = atTarget;
 	}
 
 	apply_(map, intersectionFinder, target) {
-		if (this.stage === Stages.ACTIVE)
+		if (this.stage === Stages.INACTIVE)
+			return;
+		if (this.atTarget) {
+			let delta = Vector.fromObj(target).subtract(Vector.fromObj(this.origin));
+			this.origin.setMoveDirection(delta.x, delta.y);
+		} else {
 			this.theta += this.rate;
-		[this.origin.moveDirection.x, this.origin.moveDirection.y] = thetaToVector(this.theta);
+			this.origin.setMoveDirection(...thetaToVector(this.theta));
+		}
 	}
 }
 
