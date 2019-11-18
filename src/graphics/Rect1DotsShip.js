970b88f06a4ce38d5ad6ccf3b077096ac475fefb
manuk <mahhov1@gmail.com>
2019-11-18 07:01:15 -0800

    update graphics for rect1dotsship similar to other rect4 graphics

--- src/graphics/Rect1DotsShip.js
+++ src/graphics/Rect1DotsShip.js
@@ -7,7 +7,6 @@ const RECT_POINTS = [
 	[1, 0],
 	[0, -1],
 	[-1, 0]];
-const RECT_COLOR = Color.from255(240, 200, 230).get();
 
 const DOT_SCALE = .2;
 const DOT_POS = .2;
@@ -15,14 +14,13 @@ const DOT_POINTS = PathCreator.createCirclePoints(1, 6, 0, 0);
 const DOT_COLOR = Color.from1(1, 1, 1).get();
 
 class Rect4DotsShip extends Graphics {
-	constructor(width, height) {
+	constructor(width, height, color) {
 		super();
-		this.addPath(width, height, RECT_POINTS, true,
-			{fill: 'double', color: RECT_COLOR});
+		this.addPath(width, height, RECT_POINTS, true, {fill: true, color});
 		this.addPathXY(
 			0, height * DOT_POS,
 			width * DOT_SCALE, height * DOT_SCALE,
-			DOT_POINTS, true, {fill: 'double', color: DOT_COLOR});
+			DOT_POINTS, true, {fill: true, color: DOT_COLOR});
 	}
 }
 
