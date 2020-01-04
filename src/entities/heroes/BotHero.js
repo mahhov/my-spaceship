a3ca2c94f9a6764f328e418acfcf95c7526c1670
manuk <mahhov1@gmail.com>
2020-01-03 17:26:54 -0800

    inline some bothero 1-line methods

--- src/entities/heroes/BotHero.js
+++ src/entities/heroes/BotHero.js
@@ -4,16 +4,8 @@ const Buff = require('../Buff');
 class BotHero extends Hero {
 	update(map, intersectionFinder, monsterKnowledge, goals) {
 		this.refresh();
-		this.moveControl(intersectionFinder, goals); // todo [high] inline these methods
-		this.abilityControl(map, intersectionFinder, goals);
-	}
-
-	moveControl(intersectionFinder, goals) {
 		this.updateMove(intersectionFinder, goals.movement.x, goals.movement.y, .005 * Buff.moveSpeed(this.buffs));
 		// todo [medium] speed should be parameterizable in Hero constructor.
-	}
-
-	abilityControl(map, intersectionFinder, goals) {
 		this.updateAbilities(map, intersectionFinder, goals.activeAbilitiesWanted, goals.abilitiesDirect);
 	}
 }
