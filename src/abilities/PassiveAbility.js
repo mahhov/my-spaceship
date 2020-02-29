bfa93c5a29553c7de81e104652e610dbdf2fbe28
manuk <mahhov1@gmail.com>
2020-02-29 15:24:08 -0800

    fix bug with passive abilities bieng applied regardless of disabledOk when entity was disabled

--- src/abilities/PassiveAbility.js
+++ src/abilities/PassiveAbility.js
@@ -3,7 +3,7 @@ const Ability = require('./Ability');
 class PassiveAbility extends Ability {
 	constructor(disabledOk = false) {
 		super(0, 1, 0, 0, true, 0);
-		this.disabledOk = true;
+		this.disabledOk = disabledOk;
 	}
 }
 
