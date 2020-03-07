c04360c24fa6d31ca72ccefd1e5c98695d2e274f
manuk <mahhov1@gmail.com>
2020-03-06 16:55:13 -0800

    projectileAttack to expose some constnats

--- src/abilities/ProjectileAttack.js
+++ src/abilities/ProjectileAttack.js
@@ -1,6 +1,7 @@
 const Ability = require('./Ability');
 const {setMagnitude, randVector} = require('../util/Number');
 const Projectile = require('../entities/attack/Projectile');
+const Buff = require('../entities/Buff');
 
 class ProjectileAttack extends Ability {
 	constructor() {
@@ -8,12 +9,25 @@ class ProjectileAttack extends Ability {
 	}
 
 	activate(origin, direct, map, intersectionFinder, hero) {
-		const VELOCITY = .014, SPREAD = .08, SIZE = .02, TIME = 30, DAMAGE = .1;
+		const VELOCITY = ProjectileAttack.velocity, SPREAD = .08, SIZE = .02, DAMAGE = .1;
 		let directv = setMagnitude(direct.x, direct.y, VELOCITY);
 		let randv = randVector(VELOCITY * SPREAD);
-		let projectile = new Projectile(origin.x, origin.y, SIZE, SIZE, directv.x + randv[0], directv.y + randv[1], TIME, DAMAGE, hero.friendly);
+		let projectile = new Projectile(
+			origin.x, origin.y,
+			SIZE, SIZE,
+			directv.x + randv[0], directv.y + randv[1],
+			ProjectileAttack.getTime(hero), DAMAGE,
+			hero.friendly);
 		map.addProjectile(projectile);
 		return true;
+	}
+
+	static getTime(hero) {
+		return 60 * Buff.attackRange(hero.buffs);
+	}
+
+	static get velocity() {
+		return .014;
 	}
 }
 
