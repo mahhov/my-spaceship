86501fb58759ec7368691c5989baf33c3f28d683
manuk <mahhov1@gmail.com>
2020-03-07 13:19:37 -0800

    RecordMp4.js to use blob instead of file

--- src/util/RecordMp4.js
+++ src/util/RecordMp4.js
@@ -1,16 +1,13 @@
 let record = (ms = 0) => {
 	let stream = canvas.captureStream(20);
-	let recorder = new MediaRecorder(stream);
+	let recorder = new MediaRecorder(stream, {mimeType: 'video/webm'});
 	let data = [];
 
 	recorder.addEventListener('dataavailable', event => data.push(event.data));
 
 	recorder.addEventListener('stop', () => {
-		console.log('r done', data);
-
-		let file = new File(data, '', {type: "video/mp4"});
-		let exportUrl = URL.createObjectURL(file);
-		console.log(exportUrl);
+		let blob = new Blob(data, {type: 'video/webm'});
+		let exportUrl = URL.createObjectURL(blob);
 		window.open(exportUrl, '_blank');
 	});
 
