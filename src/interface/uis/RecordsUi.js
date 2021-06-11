6ba9170af979f19a2f6f9a1c1f470fe8ebb37e10
mahhov1 <mahhov1@gmail.com>
2021-06-11 12:48:17 -0700

    remove '...' from record lines

--- src/interface/uis/RecordsUi.js
+++ src/interface/uis/RecordsUi.js
@@ -10,7 +10,7 @@ class RecordsUi extends Ui {
 		let layout = new ListUiLayout(new Coordinate(.4, Positions.UI_FIRST_ROW, .2, 0), Positions.UI_ROW_HEIGHT);
 		let valueTexts = recordsData.recordItems.map((recordItem, i) => {
 			let coordinates = layout.getCoordinates(i);
-			this.add(new UiText(coordinates.container, `${recordItem.name}...`));
+			this.add(new UiText(coordinates.container, `${recordItem.name}`));
 			let valueText = this.add(new UiText(coordinates.right, recordItem.value));
 			return [valueText, recordItem];
 		});
