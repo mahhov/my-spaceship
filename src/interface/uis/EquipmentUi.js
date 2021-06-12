3b5f470f3c9d7ecf7a500a73ea363d0b658d0fe1
mahhov1 <mahhov1@gmail.com>
2021-06-11 23:46:50 -0700

    uise grid layout for equipmentUi

--- src/interface/uis/EquipmentUi.js
+++ src/interface/uis/EquipmentUi.js
@@ -3,6 +3,7 @@ import Coordinate from '../../util/Coordinate.js';
 import UiButton from '../components/UiButton.js';
 import UiSection from '../components/UiSection.js';
 import HubUi from './HubUi.js';
+import UiGridLayout from './layouts/UiGridLayout.js';
 import Ui from './Ui.js';
 
 class EquipmentUi extends Ui {
@@ -11,7 +12,6 @@ class EquipmentUi extends Ui {
 		let coordinate = HubUi.createSection('', false, .7).coordinate;
 
 		// todo [high] use icon buttons
-		// todo [high] use grid layout
 
 		const COLUMNS = 16, ROWS = 8;
 		let buttonSize = coordinate.width / COLUMNS;
@@ -20,8 +20,7 @@ class EquipmentUi extends Ui {
 		let equippedCoordinate = coordinate.clone
 			.alignWithoutMove(Coordinate.Aligns.START)
 			.size(buttonSize * 4, buttonSize);
-		this.add(new UiSection(equippedCoordinate, 'Equipped'));
-		this.fillButtons(equippedCoordinate, buttonSize);
+		this.equippedButtons = this.createSection(equippedCoordinate, 'Equipped', 4, 1, buttonSize);
 
 		let salvageCoordinate = coordinate.clone
 			.alignWithoutMove(Coordinate.Aligns.END, Coordinate.Aligns.START)
@@ -32,32 +31,25 @@ class EquipmentUi extends Ui {
 			.shift(-1, 0)
 			.move(-Positions.MARGIN, 0)
 			.size(buttonSize * 4, buttonSize);
-		this.add(new UiSection(forgeCoordinate, 'Forge'));
-		this.fillButtons(forgeCoordinate, buttonSize);
+		this.forgeButtons = this.createSection(forgeCoordinate, 'Forge', 4, 1, buttonSize);
 
 		let inventoryCoordinate = equippedCoordinate.clone
 			.shift(0, 1)
 			.move(0, verticalMargin)
-			.size(coordinate.width, 8 * buttonSize);
-		this.add(new UiSection(inventoryCoordinate, 'Inventory'));
-		this.fillButtons(inventoryCoordinate, buttonSize);
+			.size(coordinate.width, ROWS * buttonSize);
+		this.inventoryButtons = this.createSection(inventoryCoordinate, 'Inventory', COLUMNS, ROWS, buttonSize);
 
 		let materialsCoordinate = inventoryCoordinate.clone
 			.shift(0, 1)
 			.move(0, verticalMargin);
-		this.add(new UiSection(materialsCoordinate, 'Materials'));
-		this.fillButtons(materialsCoordinate, buttonSize);
+		this.materialButtons = this.createSection(materialsCoordinate, 'Materials', COLUMNS, ROWS, buttonSize);
 	}
 
-	fillButtons(containerCoordinate, buttonSize) {
-		let columns = Math.floor(containerCoordinate.width / buttonSize);
-		let rows = Math.floor(containerCoordinate.height / buttonSize);
-		let buttonCoordinate = containerCoordinate.clone
-			.alignWithoutMove(Coordinate.Aligns.START)
-			.size(containerCoordinate.width / columns, containerCoordinate.height / rows);
-		for (let x = 0; x < columns; x++)
-			for (let y = 0; y < rows; y++)
-				this.add(new UiButton(buttonCoordinate.clone.shift(x, y), ''));
+	createSection(coordinate, sectionTitle, columns, rows, buttonSize) {
+		this.add(new UiSection(coordinate, sectionTitle));
+		let layout = new UiGridLayout(coordinate, columns, buttonSize, 0, 0);
+		return [...Array(columns * rows)].map((_, i) =>
+			this.add(new UiButton(layout.getContainerCoordinate(i), '')));
 	}
 }
 
