2e4af7c6b861a3dc4c942bc38f3607bf724854e7
mahhov1 <mahhov1@gmail.com>
2021-06-23 14:33:45 -0700

    connect techniqueUi to techniqueData

--- src/interface/uis/TechniquesUi.js
+++ src/interface/uis/TechniquesUi.js
@@ -1,6 +1,7 @@
 import {Positions} from '../../util/constants.js';
 import Coordinate from '../../util/Coordinate.js';
 import UiOutline from '../components/UiOutline.js';
+import UiPopupText from '../components/UiPopupText.js';
 import UiText from '../components/UiText.js';
 import GridLayout from '../layouts/GridLayout.js';
 import AllocationUi from './AllocationUi.js';
@@ -10,12 +11,13 @@ import Ui from './Ui.js';
 class TechniquesUi extends Ui {
 	constructor(techniqueData) {
 		super();
+		this.techniqueData = techniqueData;
 
 		let coordinate = new Coordinate(0, Positions.UI_FIRST_ROW, 1, Positions.UI_BUTTON_HEIGHT)
 			.pad(Positions.MARGIN, 0);
-		this.add(new TabsUi(coordinate.clone, techniqueData.trees.map(tree => tree.name)));
+		let tabsUi = this.add(new TabsUi(coordinate.clone, techniqueData.trees.map(tree => tree.name)));
 
-		this.add(new UiText(coordinate.clone.alignWithoutMove(Coordinate.Aligns.END), techniqueData.availableText));
+		this.availableText = this.add(new UiText(coordinate.clone.alignWithoutMove(Coordinate.Aligns.END)));
 
 		coordinate
 			.shift(0, 1)
@@ -23,11 +25,26 @@ class TechniquesUi extends Ui {
 			.size(coordinate.width, 1 - coordinate.top - Positions.MARGIN);
 		this.add(new UiOutline(coordinate.clone));
 
+		let hoverText = new UiPopupText(new Coordinate(0, 0, .22));
+
 		coordinate.pad(Positions.MARGIN);
 		let treeLayout = GridLayout.createWithFixedColumnWidth(coordinate, 4, AllocationUi.width, AllocationUi.height, Positions.MARGIN, Positions.MARGIN * 2.5);
-		techniqueData.trees[0].allocationSets.forEach((set, setIndex) =>
-			set.forEach((allocation, allocationIndex) =>
-				this.add(new AllocationUi(treeLayout.getCoordinatesRowColumn(setIndex, allocationIndex).container, allocation))));
+		this.allocationUiSets = tabsUi.uiSets = techniqueData.trees.map(tree =>
+			tree.allocationSets.flatMap((allocations, setIndex) =>
+				allocations.flatMap((allocation, allocationIndex) =>
+					this.add(AllocationUi.createAndBindAllocationUi(
+						treeLayout.getCoordinatesRowColumn(setIndex, allocationIndex).container,
+						allocation, techniqueData, hoverText)))));
+
+		this.add(hoverText);
+
+		techniqueData.on('change', () => this.refresh());
+		this.refresh();
+	}
+
+	refresh() {
+		this.allocationUiSets.forEach(allocationUis => allocationUis.forEach(allocationUi => allocationUi.updateValueText()));
+		this.availableText.text = this.techniqueData.availableText;
 	}
 }
 
--- src/util/string.js
+++ src/util/string.js
@@ -1,10 +1,10 @@
 // convert 'inc    +20% yoHow_ARE_YouToday'' to 'Inc +20% yo how are you today'
-const toUiString = string => string
+const toUiString = string => string ? string
 	.split(/[ _]+|([A-Z]+[a-z]*)/)
 	.filter(s => s)
 	.map(s => s.toLowerCase())
 	.join(' ')
-	.replace(/^./, m => m.toUpperCase());
+	.replace(/^./, m => m.toUpperCase()) : '';
 
 export {
 	toUiString,
