9aa4c22b867a304f02f08b32597ba14f587462f4
mahhov1 <mahhov1@gmail.com>
2021-06-28 08:48:39 -0700

    try building git docs x2

---
+++ docs/experiments/testPathGenerator.js
@@ -0,0 +1,13 @@
+import PathGenerator from '../src/map/PathGenerator.js';
+
+let pathGenerator = new PathGenerator(300, 300, 10);
+
+let {start, end} = pathGenerator.createPathEndpoints();
+console.log('>', start, end);
+
+for (let i = 0; i < 300; i++) {
+	let move = pathGenerator.updatePath(start, end);
+	start.x += move.x;
+	start.y += move.y;
+	console.log(start);
+}
