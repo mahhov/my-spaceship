84dc0995e32ea40b259ed3cabe9e35d23f5e5d30
mahhov1 <mahhov1@gmail.com>
2021-07-10 00:03:38 -0700

    todo lower priority

--- src/interface/uis/EquipmentUi.js
+++ src/interface/uis/EquipmentUi.js
@@ -15,7 +15,7 @@ import GridLayout from '../layouts/GridLayout.js';
 import HubUi from './HubUi.js';
 import Ui from './Ui.js';
 
-// todo [high] move these constants to data layer to be consistent with Traits
+// todo [medium] move these constants to data layer to be consistent with Traits and Technique
 const ImagePaths = {
 	Equipment: {
 		[Equipment.Types.HULL]: '../../images/equipment/spaceship.png',
