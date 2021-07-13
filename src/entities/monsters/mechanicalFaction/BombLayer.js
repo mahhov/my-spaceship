7385d80c87f47a1c586beba38cd56648d433d203
mahhov1 <mahhov1@gmail.com>
2021-07-13 13:00:09 -0700

    migrate AreaDegenLayer.js & BombLayer.js to new modules

--- src/entities/monsters/mechanicalFaction/BombLayer.js
+++ src/entities/monsters/mechanicalFaction/BombLayer.js
@@ -1,61 +1,41 @@
 import DoubleHorizDiamondShip from '../../../graphics/DoubleHorizDiamondShip.js';
 import MaterialDrop from '../../../playerData/MaterialDrop.js';
 import {Colors} from '../../../util/constants.js';
-import makeEnum from '../../../util/enum.js';
 import {PI} from '../../../util/number.js';
-import Phase from '../../../util/Phase.js';
-import Aim from '../../modulesDeprecated/Aim.js';
-import AreaDegenLayer from '../../modulesDeprecated/AreaDegenLayer.js';
-import Chase from '../../modulesDeprecated/Chase.js';
-import Cooldown from '../../modulesDeprecated/Cooldown.js';
-import Distance from '../../modulesDeprecated/Distance.js';
-import MonsterDeprecated from '.././MonsterDeprecated.js';
+import Aim from '../../modules/Aim.js';
+import AreaDegenLayer from '../../modules/AreaDegenLayer.js';
+import Chase from '../../modules/Chase.js';
+import Cooldown from '../../modules/Cooldown.js';
+import Distance from '../../modules/Distance.js';
+import Monster from '../Monster.js';
 
-const Phases = makeEnum({ONE: 0});
-
-class BombLayer extends MonsterDeprecated {
+class BombLayer extends Monster {
 	constructor(x, y) {
 		super(x, y, .04, .04, 120, 300, new MaterialDrop(1, false));
 		this.setGraphics(new DoubleHorizDiamondShip(this.width, this.height, {fill: true, color: Colors.Entity.MONSTER.get()}));
 
-		this.attackPhase = new Phase(0);
-
-		let distance = new Distance();
+		let distance = this.addModule(new Distance());
 		distance.config(this, .1, 1);
-		this.moduleManager.addModule(distance, {[Phases.ONE]: Distance.Stages.ACTIVE});
+		distance.setStage(Distance.Stages.ACTIVE);
 
-		let aim = new Aim();
+		let aim = this.addModule(new Aim());
 		aim.config(this, PI / 80, 80, .2);
-		distance.addModule(aim, {
-			0: Aim.Stages.REVERSE,
-			1: Aim.Stages.ACTIVE,
-			2: Aim.Stages.INACTIVE,
-		});
 
-		let chase = new Chase();
+		let chase = this.addModule(new Chase());
 		chase.config(this, .003, aim);
-		distance.addModule(chase, {
-			0: Chase.Stages.ACTIVE,
-			1: Chase.Stages.ACTIVE,
-			2: Chase.Stages.INACTIVE,
-		});
 
-		let cooldown = new Cooldown();
+		let cooldown = this.addModule(new Cooldown());
 		cooldown.config(80);
-		distance.addModule(cooldown, {
-			0: Cooldown.Stages.ACTIVE,
-			1: Cooldown.Stages.ACTIVE,
-			2: Cooldown.Stages.COOLDOWN,
-		});
 
-		let areaDegen = new AreaDegenLayer();
+		distance.onChangeSetModuleStages(
+			[aim, Aim.Stages.REVERSE, Aim.Stages.ACTIVE, Aim.Stages.INACTIVE],
+			[chase, Chase.Stages.ACTIVE, Chase.Stages.ACTIVE, Chase.Stages.INACTIVE],
+			[cooldown, Cooldown.Stages.ACTIVE, Cooldown.Stages.ACTIVE, Cooldown.Stages.COOLDOWN],
+		);
+
+		let areaDegen = this.addModule(new AreaDegenLayer());
 		areaDegen.config(this, .1, 200, .3);
-		cooldown.addModule(areaDegen, {
-			[Cooldown.Phases.UNTRIGGERED]: AreaDegenLayer.Stages.INACTIVE,
-			[Cooldown.Phases.TRIGGERED]: AreaDegenLayer.Stages.ACTIVE,
-		});
-
-		this.moduleManager.modulesSetStage(this.attackPhase.get());
+		cooldown.onChangeSetModuleStages([areaDegen, AreaDegenLayer.Stages.INACTIVE, AreaDegenLayer.Stages.ACTIVE]);
 	}
 }
 
